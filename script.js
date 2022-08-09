30af9802b4854ba70e02e41f643adae3bf860498
Dave Ostergren <d.william.ostergren@gmail.com>
2022-08-09 16:44:23 -0400

    got the playRound function to work as expected

--- script.js
+++ script.js
@@ -2,16 +2,57 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max)
 }
 
-function computerChoice() {
-    let answers = [
+function getComputerChoice() {
+    const answers = [
         "rock",
         "paper",
         "scissors",
     ]
     let n = getRandomInt(3)
     let choice = answers[n]
+    console.log(choice)
 
-    return console.log(choice)
+    return choice
 }
 
-computerChoice()
+function getPlayerChoice() {
+    return prompt("Rock, Paper, or scissors?").toLowerCase()
+}
+
+
+function playRound(computerSelection, playerSelection) {
+    let winner = undefined
+    if (computerSelection == playerSelection) {
+        winner = "draw"
+    } else if (computerSelection == "rock") {
+        if (playerSelection == "paper") {
+             winner = "player"
+        } else if (playerSelection == "scissors") {
+             winner = "computer"
+        }
+    } else if (computerSelection == "paper") {
+        if (playerSelection == "scissors") {
+              winner = "player"
+        } else if (playerSelection == "rock") {
+             winner = "computer"
+        }
+    } else if (computerSelection == "scissors") {
+        if (playerSelection == "rock") {
+             winner = "player"
+        } else if (playerSelection == "paper") {
+             winner = "computer"
+        }
+    }
+    return console.log(winner)
+}
+
+
+
+
+
+let computerChocie = getComputerChoice()
+let playerChoice = getPlayerChoice()
+console.log("computer " + typeof(computerChocie))
+console.log("player: " + typeof(playerChoice))
+playRound(computerChocie, playerChoice)
+
