54eab98ffab8a9ccf6280aba1204a78f0c531844
Dave Ostergren <d.william.ostergren@gmail.com>
2022-09-30 10:28:47 -0400

    got the scores to update dynamically.  not the most elegant fix, but functional.  next iteration should be to update text rather than remove and replace after each round

--- script.js
+++ script.js
@@ -22,19 +22,65 @@ function getComputerChoice() {
  // select buttons with class type "choice"
  const options = document.querySelectorAll('.option');
  const results = document.querySelector('#results');
-
- console.log(options)
-
-
- let msg = document.createElement("p")
- let text = document.createTextNode("Results go here")
- msg.appendChild(text)
- results.append(msg)
-
-// returns the id of the button which is clicked as a string
+ const playerScore = document.querySelector('#playerScore')
+ const  computerScore = document.querySelector('#computerScore')
 
 
- options.forEach(option => option.addEventListener("click", playRound)) 
+
+// playerPoints and ComputerPoints will be updated after each round.
+ let playerPoints = 0;
+ let computerPoints = 0;
+
+//  DOM handling to add scores in the appropriate scoreboard locations
+
+let pscore = document.createElement("p");
+let ptext = document.createTextNode(playerPoints);
+pscore.appendChild(ptext);
+playerScore.appendChild(pscore);
+
+let cscore = document.createElement("p");
+let ctext = document.createTextNode(computerPoints);
+cscore.appendChild(ctext);
+computerScore.appendChild(cscore);
+
+
+// update score function will use DOM manipulation to add the current score to the 
+// scoreboard container in the appropriate locations for each score after each round
+// (in other words, at the end of the the playRound function)
+
+
+function updateScore() {
+    computerScore.removeChild(computerScore.lastChild)
+    playerScore.removeChild(playerScore.lastChild)
+
+    let pscore = document.createElement("p");
+    let ptext = document.createTextNode(playerPoints);
+    pscore.appendChild(ptext);
+    playerScore.appendChild(pscore);
+
+
+    let cscore = document.createElement("p");
+    let ctext = document.createTextNode(computerPoints);
+    cscore.appendChild(ctext);
+    computerScore.appendChild(cscore);
+
+}
+
+
+
+
+
+// DOM handling for messaging.  This allows text to be displayed at the 
+// end of each round annoucning the winner
+
+ let msg = document.createElement("p");
+ let text = document.createTextNode("Results go here");
+ msg.appendChild(text);
+ results.append(msg);
+
+// getPlayChoice returns the html id of the button which is clicked as a string.  
+// this id is used later in the playRound function in order provide a string representing the players choice for that round
+
 
 function getPlayerChoice(e) {
     // results.append(msg)
@@ -45,9 +91,11 @@ function getPlayerChoice(e) {
     return e.target.id;
  }
 
-
-
- 
+ // play round takes an event (a button click in this case), which is passed to player selection.
+ //computer selection is generated randomly from a list of strings
+ // player and computer choices are compared using if statements to determine the winner
+ // the 'winner' variable is updated to player, computer, or draw depending on the comparison of choices
+ //
 
 function playRound(e) {
     let playerSelection = getPlayerChoice(e);
@@ -57,44 +105,50 @@ function playRound(e) {
         winner = "draw"
     } else if (computerSelection == "rock") {
         if (playerSelection == "paper") {
-             winner = "player"
+             winner = "player";
         } else if (playerSelection == "scissors") {
-             winner = "computer"
+             winner = "computer";
         }
     } else if (computerSelection == "paper") {
         if (playerSelection == "scissors") {
-              winner = "player"
+              winner = "player";
         } else if (playerSelection == "rock") {
-             winner = "computer"
+             winner = "computer";
         }
     } else if (computerSelection == "scissors") {
         if (playerSelection == "rock") {
-             winner = "player"
+             winner = "player";
         } else if (playerSelection == "paper") {
-             winner = "computer"
+             winner = "computer";
         }
     }
     if (winner == "draw") {
-        let msg = document.createElement("p")
-        let text = document.createTextNode("It's a draw!")
-        msg.appendChild(text)
-        results.append(msg)
+        let msg = document.createElement("p");
+        let text = document.createTextNode("It's a draw!");
+        msg.appendChild(text);
+        results.append(msg);
     } else if (winner == "player") {
-        let msg = document.createElement("p")
-        let text = document.createTextNode("You win! " + playerSelection + " beats " + computerSelection + ".")
-        msg.appendChild(text)
-        results.append(msg)
+        playerPoints += 1;
+        let msg = document.createElement("p");
+        let text = document.createTextNode("You win! " + playerSelection + " beats " + computerSelection + ".");
+        msg.appendChild(text);
+        results.append(msg);
     } else {
-        let msg = document.createElement("p")
-        let text = document.createTextNode("You lose. " + computerSelection + " beats " + playerSelection + ".")
-        msg.appendChild(text)
-        results.append(msg)
-        
-        
+        computerPoints +=1;
+        let msg = document.createElement("p");
+        let text = document.createTextNode("You lose. " + computerSelection + " beats " + playerSelection + ".");
+        msg.appendChild(text);
+        results.append(msg);     
+   
     }
-
+    updateScore()
+    console.log(playerPoints)
+    console.log(computerPoints) 
 }
 
+
+
+options.forEach(option => option.addEventListener("click", playRound)) 
 
 
 
